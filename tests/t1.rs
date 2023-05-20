52d33c3a44ccd5f027297a1ef0e1c7b4e6f6bf0b
Christian Jaeger <ch@christianjaeger.ch>
2023-05-20 14:53:51 +0200

    t1: do not use unwrap
    This is probably not the right way to do it.

--- tests/t1.rs
+++ tests/t1.rs
@@ -1,12 +1,14 @@
+use anyhow::Result;
 use anysexpr::read::{read_all, write_all};
 
 const INPUT: &[u8] = include_bytes!("t-input.scm");
 const EXPECTED: &[u8] = include_bytes!("t-expected.scm");
 
 #[test]
-fn t1() {
-    let vals = read_all(INPUT).unwrap();
+fn t1() -> Result<()> {
+    let vals = read_all(INPUT)?;
     let mut out = Vec::<u8>::new();
-    write_all(&mut out, &vals).unwrap();
+    write_all(&mut out, &vals)?;
     assert_eq!(out, EXPECTED);
+    Ok(())
 }
