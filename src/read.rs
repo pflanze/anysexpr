6ee6205b15261fb51cced565c36db93f370c4840
Christian Jaeger <ch@christianjaeger.ch>
2023-06-09 10:44:45 +0200

    Revert "read: replace trait with iter wrapper: TokenReader"
    This reverts commit 55c5c6c1fdc2407b4d864e0e3774383ffdbab379.

--- src/read.rs
+++ src/read.rs
@@ -169,20 +169,37 @@ fn dec(fuel: u32) -> Result<u32, ReadError> {
     Ok(fuel - 1)
 }
 
-pub struct TokenReader<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>>(T);
 
-impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokenReader<T> {
+pub trait TokensRead<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> {
 
     /// Read one expression. Returns None on EOF. Signals
     /// ReadError::UnexpectedClosingParen if there's no expression left in
     /// the current level.
-    pub fn read(
+    fn read(
+        &mut self,
+        depth_fuel: u32,
+    ) -> Result<Option<VValueWithPos>, ReadErrorWithPos>;
+
+    /// Read and fill a vector of values up to the expected end paren, and
+    /// return the vector and the position of a Dot, if any. Checking
+    /// whether a dot is allowed is left to the caller.
+    fn read_all(
+        &mut self,
+        opt_parenkind: Option<(Parenkind, Pos)>,
+        depth_fuel: u32,
+    ) -> Result<(Vec<VValueWithPos>, Option<Pos>), ReadErrorWithPos>;
+}
+
+
+impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokensRead<T> for T {
+
+    fn read(
         &mut self,
         depth_fuel: u32,
     ) -> Result<Option<VValueWithPos>, ReadErrorWithPos>
     {
         let get_prefixing =
-            |ts: &mut TokenReader<T>, quotepos, symname| ->
+            |ts: &mut T, quotepos, symname| ->
             Result<Option<VValueWithPos>, ReadErrorWithPos> {
                 if let Some(expr) = ts.read(dec(depth_fuel).at(quotepos)?)? {
                     Ok(Some(list2(symbol(symname).at(quotepos), expr).at(quotepos)))
@@ -190,7 +207,7 @@ impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokenReader<T>
                     Err(ReadError::MissingExpressionAfter(symname).at(quotepos))
                 }
             };
-        while let Some(TokenWithPos(t, pos)) = self.0.next().transpose()? {
+        while let Some(TokenWithPos(t, pos)) = self.next().transpose()? {
             match t {
                 Token::Dot => {
                     return Err(ReadError::ImproperlyPlacedDot.at(pos))
@@ -229,10 +246,7 @@ impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokenReader<T>
         Ok(None)
     }
     
-    /// Read and fill a vector of values up to the expected end paren, and
-    /// return the vector and the position of a Dot, if any. Checking
-    /// whether a dot is allowed is left to the caller.
-    pub fn read_all(
+    fn read_all(
         &mut self,
         opt_parenkind: Option<(Parenkind, Pos)>,
         depth_fuel: u32,
@@ -266,8 +280,7 @@ impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokenReader<T>
                             if let Some(vp) = self.read(dec(depth_fuel).at(*pos)?)? {
                                 // The next token must be a Close if we're
                                 // in a list, or none otherwise:
-                                let expecting_close = |ts: &mut TokenReader<T>, result|
-                                {
+                                let expecting_close = |ts: &mut T, result| {
                                     // Use token_read or get just one
                                     // token? Just one token: be lazy /
                                     // report the error *here* not some
@@ -276,7 +289,7 @@ impl<T: Iterator<Item = Result<TokenWithPos, ParseErrorWithPos>>> TokenReader<T>
                                     // paren check logic further down,
                                     // sigh.
                                     if let Some(TokenWithPos(t, pos)) =
-                                        ts.0.next().transpose()?
+                                        ts.next().transpose()?
                                     {
                                         match t {
                                             Token::Close(pk_end) => {
@@ -382,7 +395,8 @@ impl AnysexprFormat {
         };
         let depth_fuel = 500;
         // ^ the limit with default settings on Linux is around 1200
-        TokenReader(parse(charswithpos.into_iter(), &settings)).read(depth_fuel)
+        let mut ts = parse(charswithpos.into_iter(), &settings);
+        ts.read(depth_fuel)
     }
 
     /// Read (deserialize) all of an input stream to a sequence
@@ -401,8 +415,8 @@ impl AnysexprFormat {
         };
         let depth_fuel = 500;
         // ^ the limit with default settings on Linux is around 1200
-        let (v, maybedot) = TokenReader(
-            parse(charswithpos.into_iter(), &settings)).read_all(
+        let mut ts = parse(charswithpos.into_iter(), &settings);
+        let (v, maybedot) = ts.read_all(
             None,
             depth_fuel)?;
         if let Some(pos) = maybedot {
