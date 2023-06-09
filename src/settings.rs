e70ec5ce0f62646dc531a3f1b96da9a8088cbb09
Christian Jaeger <ch@christianjaeger.ch>
2023-06-09 15:12:29 +0200

    Use formatting in docs

--- src/settings.rs
+++ src/settings.rs
@@ -56,7 +56,7 @@ pub const GUILE_FORMAT : AnysexprFormat = AnysexprFormat {
 pub struct Modes {
     pub retain_whitespace: bool,
     pub retain_comments: bool,
-    /// false => disallow (a . b); (a . (b)) is still allowed
+    /// `false` => disallow `(a . b)`; `(a . (b))` is still allowed
     pub allow_improper_lists: bool,
 }
 
